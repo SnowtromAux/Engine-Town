2c9e2722ac4c0063cbb9f7f36f76426fa0857829
alex dimitrov <ad@auxcode.com>
2023-03-09 01:57:18 +0200

    wfedf

--- menus/Start menu/shop_objects.js
+++ menus/Start menu/shop_objects.js
@@ -1,5 +1,7 @@
 let shop_buildings = new Map();
 
+
+
 class Shop_MC{
     constructor(){
         this.count = mc_count[1];
@@ -171,7 +173,8 @@ class Shop_WS{
         if(this.count == 0){
             //Stane cherno i s katinar
             //Trqbva da nameri nai malkiqt level na koito shte poluchish nova sgrada
-            ws_card.getElementsByClassName("building-img")[0].style.cursor = "none";
+            ws_card.getElementsByClassName("status")[0].style.display = "flex";
+            ws_card.getElementsByClassName("building-img")[0].style.cursor = "auto";
             for(let i = 1;i <= mc_max_level;i++){
                 if(ws_count[i] == 1){
                     ws_card.getElementsByClassName("unlockable")[0].getElementsByTagName("label")[0].innerHTML = `${i}`;                    
@@ -186,6 +189,7 @@ class Shop_WS{
         }else{
             ws_counts.innerHTML = `${this.owned} / ${this.count}`;
             if(this.count == this.owned){
+                ws_card.getElementsByClassName("status")[0].style.display = "flex";
                 ws_card.getElementsByClassName("building-img")[0].style.cursor = "auto";
                 ws_card.style.backgroundColor = "gray";
                 ws_card.style.opacity = "0.8";
